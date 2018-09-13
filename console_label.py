dfe2121b757d59e269bb2cfc1de50f72f4149f06
LDP <ldpalcu@gmail.com>
2018-09-13 14:50:37 +0200

    add class for creating train file in client app

---
+++ console_label.py
@@ -0,0 +1,69 @@
+import pickle
+import simplejson as json
+
+
+class ConsoleLabel:
+
+    training_file_name = "training_file.json"
+
+    def __init__(self, path_to_uncertain_pairs_file):
+        self.labeled_examples = {'distinct': [], 'match': []}
+
+        with open(path_to_uncertain_pairs_file, "rb") as fp:
+            self.uncertain_pairs = pickle.load(fp)
+
+    def get_uncertain_pair(self):
+
+        record_pair = self.uncertain_pairs.pop()
+
+        self.print_pair(record_pair)
+
+        return record_pair
+
+    def print_pair(self, record_pair):
+
+        for pair in record_pair:
+            for example in pair:
+                for field in example:
+                    line = "%s : %s" % (field, example[field])
+                    print(line)
+                print("\n")
+
+        n_match = len(self.labeled_examples['match'])
+        n_distinct = len(self.labeled_examples['distinct'])
+        print("{0}/10 positive, {1}/10 negative".format(n_match, n_distinct))
+
+        print('Do these records refer to the same thing?')
+        print('(y)es / (n)o / (u)nsure / (f)inished')
+
+    def label_record_pair(self, label, record_pair):
+
+        if label == 'y':
+            self.labeled_examples['match'].append(record_pair.pop())
+        elif label == 'n':
+            self.labeled_examples['distinct'].append(record_pair.pop())
+        elif label == 'u':
+            record_pair.pop()
+        elif label == 'f':
+            print('Finished labeling')
+            self.__create_uncertain_pairs_file()
+
+    def to_json(self, python_object):
+
+        if isinstance(python_object, tuple):
+            python_object = {'__class__': 'tuple',
+                             '__value__': list(python_object)}
+        else:
+            raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+        return python_object
+
+    def __create_uncertain_pairs_file(self):
+
+        with open(self.training_file_name, "w") as fjson:
+            json.dump(
+                self.labeled_examples,
+                fjson,
+                default=self.to_json,
+                tuple_as_array=False
+            )
